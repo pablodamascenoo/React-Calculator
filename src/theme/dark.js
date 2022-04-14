a50b7593a5e6e3e1328a125d8e1ccbdd900c6410
pablodamascenoo <pablofacex@gmail.com>
2022-04-14 20:09:13 -0300

    fix: fixed themes

---
+++ src/theme/dark.js
@@ -0,0 +1,23 @@
+export default {
+  title: "dark",
+
+  backgrounds: {
+    main: "hsl(222, 26%, 31%)",
+    screen: "hsl(224, 36%, 15%)",
+    keypad: "hsl(223, 31%, 20%)",
+  },
+
+  keys: {
+    mainKeys: "hsl(225, 21%, 49%)",
+    shadowMainKeys: "hsl(224, 28%, 35%)",
+    deleteKey: "hsl(6, 63%, 50%)",
+    shadowDeleteKey: "hsl(6, 70%, 34%)",
+    secondaryKey: "hsl(30, 25%, 89%)",
+    shadowSecondaryKey: "hsl(28, 16%, 65%)",
+  },
+
+  text: {
+    white: "#fff",
+    alt: "hsl(60, 10%, 19%)",
+  },
+};
