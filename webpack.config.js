d9c37e4374c30d70a62fbcf88210305387d17a8a
pablodamascenoo <pablofacex@gmail.com>
2022-04-15 22:22:44 -0300

    fix: fixed bundle location

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "/dist"),
+    path: path.join(__dirname, "/public"),
     filename: "bundle.js",
   },
   plugins: [
